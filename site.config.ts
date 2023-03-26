edbb2e382aba89d7a0c47f770198d6ca517044c0
rtk <zjuyye@gmail.com>
2023-03-26 11:42:22 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -52,14 +52,14 @@ export default siteConfig({
   // important pages
   navigationStyle: 'default'
   // navigationStyle: 'custom',
-  // navigationLinks: [
+   navigationLinks: [
      {
        title: 'About',
        pageId: '103b5833ba4d4b70903fe3fca5f6442b'
-     },
-     {
-       title: 'Contact',
-       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
      }
-  // ]
+  //  {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+     }
+   ]
 })
